8ee21a6cb0ebc186178bacb36fff07423af09896
Sam Goddard <hello@samgoddard.co.uk>
2022-03-11 15:33:41 +0000

    font and new imagery

--- components/emblaCarousel.js
+++ components/emblaCarousel.js
@@ -17,20 +17,17 @@ export default function EmblaCarousel( {slides} ) {
   return (
     <>
       <div className="w-[65%] fixed top-0 left-0 pointer-events-none" data-scroll data-scroll-sticky data-scroll-target="#sticky">
-        <div className="w-full h-[70vh] relative overflow-hidden bg-gray-100 mt-[55px]">
-          {slides.map((item, i) => (
-            <img
-              className={`w-full h-full will-change-transform absolute inset-0 object-cover ${ i == current ? 'opacity-100' : 'opacity-0' }`}
-              src={item.image}
-              alt="A cool cat."
-              key={i}
-            />
-          ))}
+        <div className="w-full relative overflow-hidden bg-gray-100 mt-[55px]">
+          <img
+            className={`w-full will-change-transform`}
+            src={slides[current].mainImage ? slides[current].mainImage : slides[current].image}
+            alt="A cool cat."
+          />
         </div>
           
         <div className="relative w-full">
           {slides.map((item, i) => (
-            <div className={`flex uppercase text-sm pl-3 pt-1 absolute top-0 left-0 right-0 ${ i == current ? 'opacity-100 scale-100' : 'opacity-0 scale-110' }`}>
+            <div className={`flex uppercase text-sm pl-3 pt-1 absolute top-0 left-0 right-0 font-mono ${ i == current ? 'opacity-100 scale-100' : 'opacity-0 scale-110' }`}>
             <span className="w-auto m-auto">JBS.{current < 10 && ('0')}{current}</span>
             <span className="ml-4 flex-1">{slides[current].title}</span>
             <span className="flex-1">{slides[current].type}</span>
@@ -41,7 +38,7 @@ export default function EmblaCarousel( {slides} ) {
         </div>
       </div>
       <div className="flex flex-wrap p-8">
-      <div className="ml-auto w-[30%] flex flex-col">
+      <div className="ml-auto w-[30%] flex flex-col pt-20">
           {slides.map((item, i) => (
             <div className="relative flex" key={i}>
               <div className={`${item.classes} relative overflow-hidden mt-5`}>
@@ -50,7 +47,7 @@ export default function EmblaCarousel( {slides} ) {
                   src={item.image}
                   alt="A cool cat."
                   onClick={() => setCurrent(i)}
-                  data-scroll data-scroll-repeat data-scroll-call={i} data-scroll-offset="70%, 30%"
+                  data-scroll data-scroll-repeat data-scroll-call={i} data-scroll-offset="80%, 20%"
                 />
               </div>
             </div>
